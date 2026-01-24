f27cf2c3ca0679a42be02a9e1569b525194c2cb5
Superlucky Li <211234215@qq.com>
2026-01-24 22:46:38 +0800

    创建初始项目

---
+++ src/main/java/lizhuoer/agri/agri_system/AgriSystemApplication.java
@@ -0,0 +1,13 @@
+package lizhuoer.agri.agri_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lizhuoer/agri/agri_system/AgriSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lizhuoer.agri.agri_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
